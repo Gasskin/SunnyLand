4bdc066412e7360411fae74796bd3d8ec63a8160
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-05 22:15:21 +0800

    动作优化

--- Assets/Scripts/FrogAI.cs
+++ Assets/Scripts/FrogAI.cs
@@ -7,13 +7,20 @@ public class FrogAI : MonoBehaviour
     private Rigidbody2D rb;
     public Transform LeftTrans, RightTrans;
     public float speedx = 5;
-    public float speedy = 5;
+    public float speedy = 8;
+    public LayerMask ground;
 
     float left, right;
+
+    private Animator anim;
+    private Collider2D collider;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+
         left = LeftTrans.position.x;
         right = RightTrans.position.x;
         Destroy(LeftTrans.gameObject);
@@ -23,26 +30,54 @@ public class FrogAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        SwitchAnim();
     }
 
     void Move()
     {
         if (transform.localScale.x > 0)
         {
-            rb.velocity = new Vector2(-speedx, rb.velocity.y);
-            if (transform.position.x < left)
+            if (collider.IsTouchingLayers(ground))
             {
-                transform.localScale = new Vector3(-1, 1, 1);
+                rb.velocity = new Vector2(-speedx, speedy);
+                anim.SetBool("jumping", true);
             }
         }
         else
         {
-            rb.velocity = new Vector2(speedx, rb.velocity.y);
-            if (transform.position.x > right)
+            if (collider.IsTouchingLayers(ground))
             {
-                transform.localScale = new Vector3(1, 1, 1);
+                rb.velocity = new Vector2(speedx, speedy);
+                anim.SetBool("jumping", true);
             }
+        }
+    }
+
+    void ChangeFaceTo()
+    {
+        if (transform.position.x < left)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if(transform.position.x > right)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    void SwitchAnim()
+    {
+        if (anim.GetBool("jumping"))
+        {
+            if (rb.velocity.y < 0.1)
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("falling", true);
+            }
+        }
+        if (collider.IsTouchingLayers(ground) && anim.GetBool("falling")) 
+        {
+            anim.SetBool("falling", false);
         }
     }
 }
