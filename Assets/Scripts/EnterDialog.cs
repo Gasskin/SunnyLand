21426e6012fd37831f93f361080b2b87b642bbb8
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-14 22:44:07 +0800

    对话框

---
+++ Assets/Scripts/EnterDialog.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class EnterDialog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject enterDialog;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            enterDialog.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            enterDialog.SetActive(false);
+        }
+    }
+}
