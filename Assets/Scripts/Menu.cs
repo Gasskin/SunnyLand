68d0dc5aa9e33b0be4f30b3118d13c1ff108e41b
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-15 15:45:12 +0800

    暂停菜单

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject stopUi;
+    public AudioSource bg;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1)  ;
+    }
+
+    public void PauseUi()
+    {
+        Time.timeScale = 0f;
+        bg.Stop();
+        stopUi.SetActive(true);
+    }
+
+    public void Continue()
+    {
+        bg.Play();
+        Time.timeScale = 1f;
+        stopUi.SetActive(false);
     }
 }
